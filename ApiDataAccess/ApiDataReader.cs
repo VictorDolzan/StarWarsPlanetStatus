7ec0e7c18dd6f94dbd2bd538e2fd15062257a139
Victor Dolzan <victorddf5@gmail.com>
2024-03-21 19:47:53 -0300

    First commit

---
+++ ApiDataAccess/ApiDataReader.cs
@@ -0,0 +1,9 @@
+namespace StarWarsPlanetsStats.ApiDataAccess;
+
+public class ApiDataReader : IApiDataReader
+{
+    public Task<string> Read(string baseAddress, string requestUri)
+    {
+        throw new NotImplementedException();
+    }
+}
