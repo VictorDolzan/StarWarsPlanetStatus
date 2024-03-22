774a453b8e3ea4f3c707d8a84b79f85cc70d1f25
Victor Dolzan <victorddf5@gmail.com>
2024-03-21 22:09:32 -0300

    📜Implemented the dependency invertion principle 📜Created try catch to handle the exceptions

--- Program.cs
+++ Program.cs
@@ -2,11 +2,50 @@
 using StarWarsPlanetsStats.ApiDataAccess;
 using StarWarsPlanetStatus.DTOs;
 
-var apiDataReader = new ApiDataReader();
-
-var json = await apiDataReader.Read("https://swapi.dev/", "api/planets");
-
-var root = JsonSerializer.Deserialize<Root>(json);
+try
+{
+    await new StarWarsPlanetsStatsApp(
+        new ApiDataReader(), 
+        new MockStarWarsApiDataReader())
+        .Run();
+}
+catch (Exception exception)
+{
+    Console.WriteLine($"An error occurred. Exception message: {exception.Message}");
+}
 
 Console.ReadKey();
 Console.WriteLine("Hello, World!");
+
+public class StarWarsPlanetsStatsApp
+{
+    private readonly IApiDataReader _apiDataReader;
+    private readonly IApiDataReader _secondaryApiDataReader;
+
+    public StarWarsPlanetsStatsApp(IApiDataReader apiDataReader, IApiDataReader secondaryApiDataReader)
+    {
+        _apiDataReader = apiDataReader;
+        _secondaryApiDataReader = secondaryApiDataReader;
+    }
+
+    public async Task Run()
+    {
+        string? json = null;
+        try
+        {
+            json = await _apiDataReader.Read("https://swapi.dev/", "api/planets");
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"API request was unseccessful. Switching to mock data. Exception message: {ex.Message}");
+            throw;
+        }
+
+        if (json is null)
+        {
+            json = await _secondaryApiDataReader.Read("https://swapi.dev/", "api/planets");
+        }
+
+        var root = JsonSerializer.Deserialize<Root>(json);
+    }
+}
