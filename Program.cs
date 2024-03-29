30496240eccfcdbcc20b49cb67909e842aa8dccb
Victor Dolzan <victorddf5@gmail.com>
2024-03-29 17:41:50 -0300

    📜Implemented the user choice process

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ catch (Exception exception)
 }
 
 Console.ReadKey();
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Bye bye, earthling!");
 
 public class StarWarsPlanetsStatsApp
 {
@@ -54,6 +54,44 @@ public class StarWarsPlanetsStatsApp
         {
             Console.WriteLine(planet);
         }
+
+        Console.WriteLine();
+        Console.WriteLine($"The statistics of which property would you like to see?");
+        Console.WriteLine("population");
+        Console.WriteLine("diameter");
+        Console.WriteLine("surface water");
+
+        var userChoice = Console.ReadLine();
+
+        if (userChoice == "population")
+        {
+            ShowStatistics(planets, "population", planet => planet.Population);
+        }
+        else if (userChoice == "diameter")
+        {
+            ShowStatistics(planets, "diameter", planet => planet.Diameter);
+        }
+        else if (userChoice == "surface water")
+        {
+            ShowStatistics(planets, "surface water", planet => planet.SurfaceWater);
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice");
+        }
+    }
+
+    private void ShowStatistics(IEnumerable<Planet> planets, string propertyName, Func<Planet, int?> propertySelector)
+    {
+        var planetWithMaxPropertyValue =
+            planets.MaxBy(propertySelector);
+
+        Console.WriteLine($"Max {propertyName} is: {propertySelector(planetWithMaxPropertyValue)} (planet is: {planetWithMaxPropertyValue.Name})");
+            
+        var planetWithMinPropertyValue =
+            planets.MinBy(propertySelector);
+
+        Console.WriteLine($"Min {propertyName} is: {propertySelector(planetWithMinPropertyValue)} (planet is: {planetWithMinPropertyValue.Name})");
     }
 
     private IEnumerable<Planet> ToPlanets(Root? root)
